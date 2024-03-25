59ce1af3da8be494b10c02329c63e84e403f4393
pramindu123 <pramindukumaranayake999@gmail.com>
2024-03-25 10:00:14 +0530

    Initial Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleApp1
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            double total=0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+           
+            double average=total/numbers.Length;
+            Console.WriteLine($"total ={total}, average={average}");
+
         }
     }
 }
+
+
